cc3541de3036a5d9af72daf39f9e52d1949409fb
stitchkip <aalla014@fiu.edu>
2024-05-27 19:37:32 -0500

    Springboot project

---
+++ project/src/test/java/cen4010/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cen4010.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
