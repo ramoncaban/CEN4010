f15cefeb19ca7ed11ae9948093f9a7f957d0afc1
avidandrade <trejoavid@gmail.com>
2024-06-19 20:22:50 -0400

    merged Book storing code base

---
+++ project/src/main/java/CEN4010/project/Book_Browsing_and_Sorting/Service/BookService.java
@@ -0,0 +1,29 @@
+package CEN4010.project.Book_Browsing_and_Sorting.Service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import CEN4010.project.Book_Browsing_and_Sorting.Models.Books;
+import CEN4010.project.Book_Browsing_and_Sorting.Repos.BookRepository;
+
+@Service
+public class BookService {
+    
+    @Autowired
+    private BookRepository bookRepository;
+    
+    public List<Books> getBooksByGenre(String genre){
+        return bookRepository.findByGenre(genre);
+    }
+
+    public List<Books> getTop10SoldBooks(){
+        return bookRepository.findTop10ByOrderBySoldcopiesDesc();
+    }
+
+    
+   
+ 
+
+    
+}
