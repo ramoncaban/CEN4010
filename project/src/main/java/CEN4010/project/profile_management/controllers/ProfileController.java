b39da8d310ebbbb85b89dcb1903d833ddd125bda
stitchkip <aalla014@fiu.edu>
2024-06-23 23:14:03 -0500

    update

--- project/src/main/java/CEN4010/project/profile_management/controllers/ProfileController.java
+++ project/src/main/java/CEN4010/project/profile_management/controllers/ProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,14 +44,20 @@ public class ProfileController {
         return new ResponseEntity<>(profileMapper.mapTo(savedProfile), HttpStatus.CREATED);
     }
 
-    // @PutMapping(path = "/profile/{username}")
-    // public ResponseEntity<ProfileDto> updateProfile(@PathVariable String username, @RequestBody ProfileDto profile) {
-    //     ProfileEntity profileEntity = profileMapper.mapFrom(profile);
-    //     ProfileEntity updatedProfile = profileService.updateProfileUsername(username, profileEntity);
-    //     if (updatedProfile == null) {
-    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    //     }
-    //     return new ResponseEntity<>(profileMapper.mapTo(updatedProfile), HttpStatus.OK);
-    // }
-}
+    @PutMapping(path = "/profile/{username}")
+    public ResponseEntity<ProfileDto> updateProfile(@PathVariable String username, @RequestBody ProfileDto profile) {
+        ProfileEntity existingProfile = profileService.getProfileUsername(username);
+        if (existingProfile == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
+        ProfileEntity profileEntity = profileMapper.mapFrom(profile);
+        profileEntity.setEmailAddress(existingProfile.getEmailAddress());
+
+        ProfileEntity updatedProfile = profileService.updateProfileUsername(username, profileEntity);
+        if (updatedProfile == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(profileMapper.mapTo(updatedProfile), HttpStatus.OK);
+    }
+}
