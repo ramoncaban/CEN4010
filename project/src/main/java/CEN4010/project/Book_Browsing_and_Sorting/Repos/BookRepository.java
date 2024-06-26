ddb8e4678c596478ee4d8ef12f73568ff8cc04b4
avidandrade <trejoavid@gmail.com>
2024-06-26 10:57:13 -0400

    Implemented calling third part of feature 1.

--- project/src/main/java/CEN4010/project/Book_Browsing_and_Sorting/Repos/BookRepository.java
+++ project/src/main/java/CEN4010/project/Book_Browsing_and_Sorting/Repos/BookRepository.java
@@ -1,6 +1,7 @@
 package CEN4010.project.Book_Browsing_and_Sorting.Repos;
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import CEN4010.project.Book_Browsing_and_Sorting.Models.Books;
@@ -10,6 +11,7 @@ import CEN4010.project.Book_Browsing_and_Sorting.Models.Books;
 public interface BookRepository extends JpaRepository <Books,Long> {
     List <Books> findByGenre(String genre);
     List <Books> findTop10ByOrderBySoldcopiesDesc();
-   
+    List <Books> findByRatingGreaterThan(double rating);
+    List <Books> findByRating(double rating);
 }
 
